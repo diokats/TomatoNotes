6266e10a4205a2c0be62fddf67c5b97e4042f02f
Dionisis <dioktys@gmail.com>
2022-01-01 22:52:31 +0200

    Add project files.

---
+++ TomatoNotesWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TomatoNotesWeb.Models;
+
+namespace TomatoNotesWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Note> Notes { get; set; }
+    }
+}
+
---
+++ TomatoNotesWeb/Models/Note.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TomatoNotesWeb.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]  
+        public string Notes { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
